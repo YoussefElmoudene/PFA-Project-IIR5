7ae737330994c72040ae0343d5be9a56d732762c
YoussefElmoudene <elmoudene.ysf@gmail.com>
2023-12-21 14:01:47 +0100

    first commit

---
+++ travel-management-backend/src/main/java/ma/emsi/travelmanagement/TravelManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package ma.emsi.travelmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelManagementBackendApplication.class, args);
+	}
+
+}
---
+++ travel-management-backend/src/test/java/ma/emsi/travelmanagement/TravelManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsi.travelmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
